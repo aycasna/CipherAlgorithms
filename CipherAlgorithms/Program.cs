08a1cb9bbe48caad47666ab2f96164e20ff8b4c6
aycasna <aycasena168@gmail.com>
2023-12-15 03:42:05 +0300

    vernam and rsa added

--- CipherAlgorithms/Program.cs
+++ CipherAlgorithms/Program.cs
@@ -1,41 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Data;
+using System.Diagnostics.Metrics;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace CipherAlgorithms
 {
-    /*
-    public class Variables
-    {
-        //public static char[] alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        
-
-    }
-    */
+   
 
     class Program
 	{
         
         static void Main(string[] args)
 		{
-            //string[] alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+            
             string choice = "";
             string text = "";
-            Console.WriteLine("\n\t\tCipher");
+            Console.WriteLine("\t\t=====================================");
+            Console.WriteLine("\t\t\tCipher Methods");
             
 
-            Console.WriteLine("(1) Rot13 Cipher\n(2) Autokey Cipher\n(3) Random Autokey ROT13 (RAKROT13)\n(4) RSA");
+            Console.WriteLine("\t\t\t(1) Rot13\n\t\t\t(2) Autokey\n\t\t\t(3) Vernam\n\t\t\t(4) RSA");
+            Console.WriteLine("\t\t=====================================");
             string cipherMethod = Console.ReadLine();
 
             if (cipherMethod == "4")
             {
-                Console.WriteLine("\n\t(1) Generate public and private key\n\t(2) Encrypt\n\t(3)Decrypt");
+                Console.WriteLine("\n\t(1) Generate public and private key\n\t(2) Encrypt\n\t(3) Decrypt");
                 choice = Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Press 1 for encryption or 2 for decryption");
+                Console.WriteLine("\n\tPress 1 for encryption or 2 for decryption");
                 choice = Console.ReadLine();
 
                 Console.Write("Enter the text: ");
@@ -50,35 +47,42 @@ namespace CipherAlgorithms
             {
                 case ("1", "1"):
                     text = ROT13.ROT13_EncryptDecrypt(text);
-                    Console.WriteLine(text);
+                    Console.WriteLine($"Cipher text: {text}");
                     Main(args);
                     break;                 
                 case ("1", "2"):
                     text = ROT13.ROT13_EncryptDecrypt(text);
-                    Console.WriteLine(text);
+                    Console.WriteLine($"Plain text: {text}");
                     Main(args);
                     break;
                 case ("2", "1"):
                     Console.Write("Enter key: ");
                     string key = Console.ReadLine();
                     text = Autokey.Autokey_Encrypt(text, key);
-                    Console.WriteLine(text);
+                    Console.WriteLine($"Cipher text: {text}");
                     Main(args);
                     break;
                 case ("2", "2"):
                     Console.Write("Enter key: ");
                     key = Console.ReadLine();
                     text = Autokey.Autokey_Decrypt(text, key);
-                    Console.WriteLine(text);
+                    Console.WriteLine($"Plain text: {text}");
                     Main(args);
                     break;
                 case ("3", "1"):
-                    text = RAKROT13.RAKROT13_Encrypt(text);
-                    Console.WriteLine(text);
+                    Console.Write("Enter key: ");
+                    key = Console.ReadLine();
+                    text = Vernam.Vernam_Encrypt(text, key);
+                    Console.WriteLine($"Cipher text: {text}");
                     Main(args);
                     break;
                 case ("3", "2"):
-                    //rakrot13 decrypt
+                    Console.Write("Enter key: ");
+                    char charKey = Convert.ToChar(Console.ReadLine());
+                    int num = Convert.ToInt32(text);
+                    text = Vernam.Vernam_Decrypt(num, charKey);
+                    Console.WriteLine($"Plain text: {text}");
+                    Main(args);
                     break;
                 case ("4", "1"):
                     double P, Q;
@@ -86,6 +90,7 @@ namespace CipherAlgorithms
                     
                     double n, e, d;
                     (n, e, d) = RSA.RSA_GenerateKeys(P, Q);
+                    Main(args);
                     break;
                 case ("4", "2"):
                     Console.Write("Enter the plain text: ");
@@ -94,16 +99,28 @@ namespace CipherAlgorithms
                     double publicKeyN = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Enter public key e: ");
                     double publicKeyE = Convert.ToDouble(Console.ReadLine());
+
                     text = RSA.RSA_Encrypt(text, publicKeyN, publicKeyE);
-                    Console.WriteLine(text);
+                    Console.WriteLine($"Cipher text: {text}");
+
                     Main(args);
                     break;
                 case ("4", "3"):
                     Console.Write("Enter the cipher text: ");
                     text = Console.ReadLine();
-                    Console.WriteLine("Enter private key d: ");
-                    double privateKeyD = Console.Read();
-                    //rsa decrypt
+
+                    Console.Write("Enter public key n: ");
+                    publicKeyN = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter private key d: ");
+                    double privateKeyD = Convert.ToDouble(Console.ReadLine());
+
+                    
+
+                    text = RSA.RSA_Decrypt(text, publicKeyN, privateKeyD);
+                    Console.WriteLine($"Plain text: {text}");
+                    Main(args);
+
                     break;
                 default:
                     break;
@@ -189,69 +206,57 @@ namespace CipherAlgorithms
         }
     }
 
-    //Random Autokey ROT13
-    static class RAKROT13 
+    //Vernam cipher
+    static class Vernam 
     {
         private static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        public static string RAKROT13_Encrypt(string plainText)
+
+        public static string Vernam_Encrypt(string plainText, string key)
         {
 
-            Random random = new Random();
-            int keyLen = random.Next(4, 10);
-            int randValue;
-            string key = "";
-            char letter;
-            for (int i = 0; i < keyLen; i++)
-            {
-                randValue = random.Next(0, 26);
-                letter = Convert.ToChar(randValue + 65); //generating random character by converting the random number to a character.
-                key = key + letter;
-            }
-
-            string randomAutokeyMessage = "";
+            string cipherText = "";
+            
             for (int i = 0; i < plainText.Length; i++)
             {
-                string newKey = key + plainText;
-                Console.WriteLine($"KEY: {newKey}");
-                int first = alphabet.IndexOf(plainText[i]);
-                int second = alphabet.IndexOf(newKey[i]);
-                int total = (first + second) % 26;
-                randomAutokeyMessage += alphabet[total];
-            }
+                int asciiValuePlainText = (int)plainText[i];
+                int asciiValueKey = (int)key[i];
 
-            
-            char[] array = randomAutokeyMessage.ToCharArray();
-            for (int i = 0; i < array.Length; i++)
-            {
-                int number = (int)array[i];
-                if (number >= 'a' && number <= 'z')
-                {
-                    if (number > 'm')
-                    {
-                        number -= 13;
-                    }
-                    else
-                    {
-                        number += 13;
-                    }
-                }
-                else if (number >= 'A' && number <= 'Z')
-                {
-                    if (number > 'M')
-                    {
-                        number -= 13;
-                    }
-                    else
-                    {
-                        number += 13;
-                    }
-                }
-                array[i] = (char)number;
+                int xorValue = asciiValuePlainText ^ asciiValueKey;
+                
+
+                //string binaryString = Convert.ToString(xorValue, 2).PadLeft(8, '0');
+                Console.WriteLine($"decimal value {i+1}: {xorValue}");
+                char asciiChar = Convert.ToChar(xorValue);
+ 
+
+                cipherText += asciiChar;
+
             }
-            return new string(array);
+            return cipherText;
 
         }
+        public static string Vernam_Decrypt(int cipherText, char key)
+        {
+            string plainText = "";
 
+            //int asciiValueCipherText = Convert.ToInt32(cipherText, 2);
+            int asciiValueCipherText = cipherText;
+            int asciiValueKey = (int)key;
+
+            int xorValue = asciiValueCipherText ^ asciiValueKey;
+            char asciiChar = Convert.ToChar(xorValue);
+            Console.WriteLine($"decimal value: {xorValue}");
+
+
+
+
+            plainText += asciiChar;
+
+            
+
+
+            return plainText;
+        }
     }
 
 
@@ -281,7 +286,7 @@ namespace CipherAlgorithms
             }
             while (!IsPrime(t1.Item2) || t1.Item2 == t1.Item1);//prim2 prim2 prime1
             
-            Console.WriteLine($"Prime 1: {t1.Item1}, Prime 2: {t1.Item2}");//prime1 prime2
+            //Console.WriteLine($"Prime 1: {t1.Item1}, Prime 2: {t1.Item2}");//prime1 prime2
             //return new (double, double)(prime1, prime2);
             return (t1.Item1, t1.Item2);
             
@@ -289,7 +294,7 @@ namespace CipherAlgorithms
 
         private static int GenerateRandomNumber()
         {
-            return rng.Next(50, 200);
+            return rng.Next(1, 10);
         }
 
         private static bool IsPrime(double number)
@@ -337,53 +342,94 @@ namespace CipherAlgorithms
                 else
                     e++;
             }
-
-            int k = 2; //constant value
-            double d = (1 + (k * phi)) / e;
+            double d = 0;
             
+            for (d = 0; d < phi; d++)
+            {
+                if ((d * e) % phi == 1)
+                {
+                    break;
+                }
+            }
+            //int k = 2; //constant value
+            //double d = (1 + (k * phi)) / e;
 
-            Console.WriteLine($"\n\tPublic keys => n = {n}, e = {e}\n\tPrivate key => d = {d}");
+
+            Console.WriteLine($"\nPublic keys => n = {n}, e = {e}\nPrivate key => d = {d}");
             return (n, e, d);
         }
 
         public static string RSA_Encrypt(string plainText, double n, double e)
         {
-            double Encrypt(double message)
-            {
-                
-                double cipherText = 1;
-                
-                cipherText = Math.Pow(message, e);
-                Console.WriteLine($"cipherText exponent 5={cipherText}");
-                cipherText %= n;
-                Console.WriteLine($"cipherText % 5={cipherText}");
-                return cipherText;
-            }
 
-            double message = 1;
             List<double> cipherTextList = new List<double>();
-            
 
-            ;
-            foreach(char letter in plainText) 
+            for (int i = 0; i < plainText.Length; i++)
             {
+                int indexOfPlainText = alphabet.IndexOf(plainText[i]);
+                Console.WriteLine($"index of plaintext {indexOfPlainText}");
+                double message = Math.Pow(indexOfPlainText, e);
+                message %= n;
+                cipherTextList.Add(message);
+            }
+            
+            
+            string cipherText = string.Join("", cipherTextList);
+            int cipherTextIndex = 0;
+            
+            for (int i = 0; i< cipherText.Length; i++)
+            {
+                cipherTextIndex = int.Parse(cipherText);
                 
-
-                cipherTextList.Add(Encrypt((double)letter));
-
             }
 
 
-            string cipherText = string.Join("",cipherTextList);
+            char cipherTextChar = alphabet[cipherTextIndex];
 
 
+
+            cipherText = cipherTextChar.ToString();
+            
             return cipherText;
+        }
+
+
+
+        public static string RSA_Decrypt(string cipherText, double n, double d)
+        {
+            List<double> plainTextList = new List<double>();
+            for (int i = 0; i<cipherText.Length; i++)
+            {
+                int indexOfCipherText = alphabet.IndexOf(cipherText[i]);//4
+                double message = Math.Pow(indexOfCipherText, d);
+                message %= n;
+                
+                    
+                plainTextList.Add(message);
+            }
+
+            string plainText = string.Join("", plainTextList);
+            int plainTextIndex = int.Parse(plainText);
+            char plainTextChar = alphabet[plainTextIndex];
+
+            plainText = plainTextChar.ToString();
+
+
+            
+            return plainText;
+            
+        }
+
+        public static List<double> storeAndAccessCipherTextList(List<double> cipherTextList)
+        {
+            return cipherTextList;
+            
         }
 
 
 
             
      }
-
+    
 }
 
